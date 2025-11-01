5d23269d516faf489fe0859d5922d6097c86139c
assiantadarsh <adarshbhai917963@gmail.com>
2025-11-01 22:54:45 +0530

    Add files via upload

---
+++ while true.py
@@ -0,0 +1,13 @@
+#it is infinite loop to break a while loop break statement is used :
+while True:
+    a = int(input("enter a:"))
+    b = int(input("enter b:"))
+    print(a+b)
+    repeat = str(input("do you want to repeat the programme:"))
+    if repeat =="yes":
+        break
+
+
+
+
+
